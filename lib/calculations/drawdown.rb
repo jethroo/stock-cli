0fe6ae93f47261064d1c98690824f2efcb22ef43
jethroo <cwirth79@web.de>
2018-04-21 14:19:28 +0200

    refactored big method in small chunks§

--- lib/calculations/drawdown.rb
+++ lib/calculations/drawdown.rb
@@ -16,55 +16,71 @@ module Calculations
 
     def drawdowns
       return unless dataset
+      reset_calculation
+
+      dataset.each do |day|
+        check_day(day)
+      end
+      @drawdowns
+    end
+
+    private
+
+    def check_day(day)
+      init_peak_before_largest_drop(day)
+      check_new_peak(day)
+      init_lowest_before_new_high(day)
+      check_new_low(day)
+      add_drawdown
+      @new_low, @new_peak = false
+    end
+
+    def init_peak_before_largest_drop(day)
+      return unless peak_before_largest_drop.nil?
+      @peak_before_largest_drop = day.high
+      @peak_day = day.date
+      @new_peak = true
+    end
+
+    def check_new_peak(day)
+      return if day.high < peak_before_largest_drop
+      @peak_before_largest_drop = day.high
+      @lowest_before_new_high = nil
+      @peak_day = day.date
+      @new_peak = true
+    end
+
+    def check_new_low(day)
+      return if lowest_before_new_high < day.low
+      @lowest_before_new_high = day.low
+      @new_low = true
+      @low_day = day.date
+    end
+
+    def init_lowest_before_new_high(day)
+      return unless lowest_before_new_high.nil?
+      @lowest_before_new_high = day.low
+      @new_low = true
+      @low_day = day.date
+    end
+
+    def add_drawdown
+      return unless new_peak || new_low
+      @drawdowns << build_summary(
+        [peak_day, peak_before_largest_drop, lowest_before_new_high,
+         low_day]
+      )
+    end
+
+    def reset_calculation
       @peak_before_largest_drop = nil
       @lowest_before_new_high = nil
       @new_peak = false
       @new_low = false
       @peak_day = nil
       @low_day = nil
-
-      drawdowns = []
-
-      dataset.each do |day|
-        if peak_before_largest_drop.nil?
-          @peak_before_largest_drop = day.high
-          @peak_day = day.date
-          @new_peak = true
-        end
-
-        if day.high > peak_before_largest_drop
-          @peak_before_largest_drop = day.high
-          @lowest_before_new_high = nil
-          @peak_day = day.date
-          @new_peak = true
-        end
-
-        if lowest_before_new_high.nil?
-          @lowest_before_new_high = day.low
-          @new_low = true
-          @low_day = day.date
-        end
-
-        if lowest_before_new_high > day.low
-          @lowest_before_new_high = day.low
-          @new_low = true
-          @low_day = day.date
-        end
-
-        if new_peak || new_low
-
-          drawdowns << build_summary(
-            [peak_day, peak_before_largest_drop, lowest_before_new_high,
-             low_day]
-          )
-        end
-
-        @new_low, @new_peak = false
-      end
-      drawdowns
+      @drawdowns = []
     end
-
-    private
 
     def build_summary(values)
       Struct::DrawdownSummary.new.tap do |summary|
