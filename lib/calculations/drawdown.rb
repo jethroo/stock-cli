14368ea1c5b458bd034d0edbd82e062fd519747a
jethroo <cwirth79@web.de>
2018-04-21 09:59:30 +0200

    provide better structur via grouping classes by modules

--- lib/calculations/drawdown.rb
+++ lib/calculations/drawdown.rb
@@ -1,58 +1,60 @@
 # frozen_string_literal: true
 
-# class for calculating drawdowns in an dataset
-class Drawdown
-  attr_reader :data
+module Calculations
+  # class for calculating drawdowns in an dataset
+  class Drawdown
+    attr_reader :data
 
-  def initialize(data)
-    @data = data
-  end
-
-  def drawdowns
-    return unless data && data['data']
-    peak_before_largest_drop = nil
-    lowest_before_new_high = nil
-    new_peak = false
-    new_low = false
-    peak_day = nil
-    low_day = nil
-
-    drawdowns = []
-
-    data['data'].each do |day|
-      if peak_before_largest_drop.nil?
-        peak_before_largest_drop = day[2]
-        peak_day = day[0]
-        new_peak = true
-      end
-
-      if day[2] > peak_before_largest_drop
-        peak_before_largest_drop = day[2]
-        lowest_before_new_high = nil
-        peak_day = day[0]
-        new_peak = true
-      end
-
-      if lowest_before_new_high.nil?
-        lowest_before_new_high = day[3]
-        new_low = true
-        low_day = day[0]
-      end
-
-      if lowest_before_new_high > day[3]
-        lowest_before_new_high = day[3]
-        new_low = true
-        low_day = day[0]
-      end
-
-      if new_peak || new_low
-        drawdowns << [
-          peak_day, peak_before_largest_drop, lowest_before_new_high, low_day
-        ]
-      end
-
-      new_low, new_peak = false
+    def initialize(data)
+      @data = data
     end
-    drawdowns
+
+    def drawdowns
+      return unless data && data['data']
+      peak_before_largest_drop = nil
+      lowest_before_new_high = nil
+      new_peak = false
+      new_low = false
+      peak_day = nil
+      low_day = nil
+
+      drawdowns = []
+
+      data['data'].each do |day|
+        if peak_before_largest_drop.nil?
+          peak_before_largest_drop = day[2]
+          peak_day = day[0]
+          new_peak = true
+        end
+
+        if day[2] > peak_before_largest_drop
+          peak_before_largest_drop = day[2]
+          lowest_before_new_high = nil
+          peak_day = day[0]
+          new_peak = true
+        end
+
+        if lowest_before_new_high.nil?
+          lowest_before_new_high = day[3]
+          new_low = true
+          low_day = day[0]
+        end
+
+        if lowest_before_new_high > day[3]
+          lowest_before_new_high = day[3]
+          new_low = true
+          low_day = day[0]
+        end
+
+        if new_peak || new_low
+          drawdowns << [
+            peak_day, peak_before_largest_drop, lowest_before_new_high, low_day
+          ]
+        end
+
+        new_low, new_peak = false
+      end
+      drawdowns
+    end
   end
 end
--- spec/datasources/quandl_spec.rb
+++ spec/datasources/quandl_spec.rb
@@ -2,26 +2,28 @@
 
 require File.expand_path '../spec_helper.rb', __dir__
 
-describe Quandl do
-  subject(:quandl) do
-    described_class.new('AAPL', Date.parse('2018-01-01'),
-                        Date.parse('2018-01-05'))
-  end
-
-  describe '#pull_data' do
-    let(:response) { instance_double('response', parsed_response: {}) }
-    let(:quandl_secrets) { { 'api_key' => 'an_api_key' } }
-
-    before do
-      allow(YAML).to receive(:load_file).and_return(quandl_secrets)
-      allow(HTTParty).to receive(:get).and_return(response)
+module Datasources
+  describe Quandl do
+    subject(:quandl) do
+      described_class.new('AAPL', Date.parse('2018-01-01'),
+                          Date.parse('2018-01-05'))
     end
 
-    it 'pulls data from quandl' do
-      expect(quandl.pull_data).to eq(nil)
-      expect(HTTParty).to have_received(:get)
-        .with('https://www.quandl.com/api/v3/datasets/EOD/AAPL.json?start_date'\
-          '=2018-01-01&end_date=2018-01-05&api_key=an_api_key&order=asc')
+    describe '#pull_data' do
+      let(:response) { instance_double('response', parsed_response: {}) }
+      let(:quandl_secrets) { { 'api_key' => 'an_api_key' } }
+
+      before do
+        allow(YAML).to receive(:load_file).and_return(quandl_secrets)
+        allow(HTTParty).to receive(:get).and_return(response)
+      end
+
+      it 'pulls data from quandl' do
+        expect(quandl.pull_data).to eq(nil)
+        expect(HTTParty).to have_received(:get)
+          .with('https://www.quandl.com/api/v3/datasets/EOD/AAPL.json?start_'\
+            'date=2018-01-01&end_date=2018-01-05&api_key=an_api_key&order=asc')
+      end
     end
   end
 end
