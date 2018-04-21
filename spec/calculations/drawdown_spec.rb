6f5b2dcf0da85dc9a84a801b28f53cbbfadc68b0
jethroo <cwirth79@web.de>
2018-04-21 13:44:55 +0200

    change test sample data to cover all cases

--- spec/calculations/drawdown_spec.rb
+++ spec/calculations/drawdown_spec.rb
@@ -18,19 +18,28 @@ module Calculations
       )
     end
 
+    # changed from sample data to cover lowest before new high case
     let(:day_3) do
       Models::DayEntry.new(
-        date: '2018-01-04', open: nil, high: 173.47, low: 172.08, close: nil
+        date: '2018-01-04', open: nil, high: 173.47, low: 168.08, close: nil
+      )
+    end
+
+    let(:day_4) do
+      Models::DayEntry.new(
+        date: '2018-01-05', open: nil, high: 175.37, low: 173.05, close: nil
       )
     end
 
     describe '#drawdowns' do
-      let(:dataset) { [day_1, day_2, day_3] }
+      let(:dataset) { [day_1, day_2, day_3, day_4] }
 
       it 'should find the drawdows correctly' do
-        expect(drawdown.drawdowns.count).to eq(2)
-        expect(drawdown.drawdowns.first.percentage).to be_within(0.001).of(1.8)
-        expect(drawdown.drawdowns.last.percentage).to be_within(0.001).of(1.5)
+        expect(drawdown.drawdowns.count).to eq(4)
+        expect(drawdown.drawdowns[0].percentage).to be_within(0.001).of(1.8)
+        expect(drawdown.drawdowns[1].percentage).to be_within(0.001).of(1.5)
+        expect(drawdown.drawdowns[2].percentage).to be_within(0.001).of(3.8)
+        expect(drawdown.drawdowns[3].percentage).to be_within(0.001).of(1.3)
       end
     end
   end
